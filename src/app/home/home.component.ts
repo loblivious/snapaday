0dcf18d50db8b8e9d36bbfd392f59f3916ad0d22
Ronald Luo <loblivious.ee@gmail.com>
2023-05-21 17:20:13 +0800

    only allow one photo per day

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,28 +4,33 @@ import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { PhotoService } from './data-access/photo.service';
 import { DomSanitizer } from '@angular/platform-browser';
-import { map } from 'rxjs';
+import { combineLatest, map } from 'rxjs';
 import { PhotoListComponentModule } from './ui/photo-list/photo-list.component';
 
 @Component({
   selector: 'app-home',
   template: `
-    <ion-header>
-      <ion-toolbar color="danger">
-        <ion-title>Snapaday</ion-title>
-        <ion-buttons slot="end">
-          <ion-button (click)="photoService.takePhoto()">
-            <ion-icon name="camera-outline" slot="icon-only"></ion-icon>
-          </ion-button>
-        </ion-buttons>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content>
-      <app-photo-list
-        *ngIf="photos$ | async as photos"
-        [photos]="photos"
-      ></app-photo-list>
-    </ion-content>
+    <ng-container *ngIf="vm$ | async as vm">
+      <ion-header>
+        <ion-toolbar color="danger">
+          <ion-title>Snapaday</ion-title>
+          <ion-buttons slot="end">
+            <ion-button
+              (click)="photoService.takePhoto()"
+              [disabled]="vm.hasTakenPhotoToday"
+            >
+              <ion-icon name="camera-outline" slot="icon-only"></ion-icon>
+            </ion-button>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>
+        <app-photo-list
+          *ngIf="photos$ | async as photos"
+          [photos]="photos"
+        ></app-photo-list>
+      </ion-content>
+    </ng-container>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -39,6 +44,16 @@ export class HomeComponent {
         ),
       }))
     )
+  );
+
+  vm$ = combineLatest([
+    this.photos$,
+    this.photoService.hasTakenPhotoToday$,
+  ]).pipe(
+    map(([photos, hasTakenPhotoToday]) => ({
+      photos,
+      hasTakenPhotoToday,
+    }))
   );
 
   constructor(
