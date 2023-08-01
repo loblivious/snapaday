7d27a64622617e148f58d1d9279a1484f2b82045
B10054 <yjluo@tdcc.com.tw>
2023-08-01 17:34:01 +0800

    taking photos with the camera api

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,28 +2,58 @@ import { IonicModule } from '@ionic/angular';
 import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { PhotoService } from './data-access/photo.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { map } from 'rxjs';
+import { PhotoListComponentModule } from './ui/photo-list/photo-list.component';
 
 @Component({
   selector: 'app-home',
   template: `
     <ion-header>
-      <ion-toolbar>
-        <ion-title>Home</ion-title>
+      <ion-toolbar color="danger">
+        <ion-title>Snapaday</ion-title>
+        <ion-buttons slot="end">
+          <ion-button (click)="photoService.takePhoto()">
+            <ion-icon name="camera-outline" slot="icon-only"></ion-icon>
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
-
-    <ion-content></ion-content>
+    <ion-content>
+      <app-photo-list
+        *ngIf="photos$ | async as photos"
+        [photos]="photos"
+      ></app-photo-list>
+    </ion-content>
   `,
   styles: [``],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  photos$ = this.photoService.photos$.pipe(
+    map((photos) =>
+      photos.map((photo) => ({
+        ...photo,
+        safeResourceUrl: this.sanitizer.bypassSecurityTrustResourceUrl(
+          photo.path
+        ),
+      }))
+    )
+  );
+
+  constructor(
+    protected photoService: PhotoService,
+    private sanitizer: DomSanitizer
+  ) {}
+}
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     IonicModule,
     CommonModule,
+    PhotoListComponentModule,
     RouterModule.forChild([
       {
         path: '',
--- src/app/home/ui/photo-list/photo-list.component.ts
+++ src/app/home/ui/photo-list/photo-list.component.ts
@@ -1,13 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  NgModule,
+} from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { Photo } from 'src/app/shared/interfaces/photo';
 
 @Component({
   selector: 'app-photo-list',
-  template: ``,
+  template: `
+    <ion-list lines="none">
+      <ion-item *ngFor="let photo of photos; trackBy: trackByFn">
+        <img [src]="photo.safeResourceUrl" />
+        <ion-badge slot="end" color="light">
+          {{ photo.dateTaken }}
+        </ion-badge>
+      </ion-item>
+    </ion-list>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PhotoListComponent {}
+export class PhotoListComponent {
+  @Input() photos: Photo[] = [];
+
+  trackByFn(index: number, photo: Photo) {
+    return photo.name;
+  }
+}
 
 @NgModule({
   declarations: [PhotoListComponent],
