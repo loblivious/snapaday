96a4275ded6f113107d15386e7b01b74908a33e3
B10054 <yjluo@tdcc.com.tw>
2023-08-02 17:28:12 +0800

    creating a slideshow - halfway

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -1,34 +1,63 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  NgModule,
+} from '@angular/core';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { BehaviorSubject, concatMap, delay, from, of, switchMap } from 'rxjs';
+import { Photo } from '../shared/interfaces/photo';
 
 @Component({
   selector: 'app-slideshow',
   template: `
     <ion-header>
-      <ion-toolbar>
-        <ion-title></ion-title>
+      <ion-toolbar color="danger">
+        <ion-title>Play</ion-title>
+        <ion-buttons slot="end">
+          <ion-button (click)="modalControl.dismiss()">
+            <ion-icon name="close" slot="icon-only"></ion-icon>
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
-
     <ion-content></ion-content>
   `,
+  styles: [
+    `
+      :host {
+        height: 100%;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SlideshowComponent {}
+export class SlideshowComponent {
+  currentPhotos$ = new BehaviorSubject<Photo[]>([]);
+  currentPhoto$ = this.currentPhotos$.pipe(
+    // Emit one photo at a time
+    switchMap((photos) => from(photos)),
+    concatMap((photo) =>
+      // Create a new stream for each individual photo
+      of(photo).pipe(
+        // Creating a stream for each individual photo
+        // to delay the start of the stream
+        delay(500)
+      )
+    )
+  );
+
+  @Input() set photos(value: Photo[]) {
+    this.currentPhotos$.next([...value].reverse());
+  }
+
+  constructor(protected modalControl: ModalController) {}
+}
 
 @NgModule({
   declarations: [SlideshowComponent],
-  imports: [
-    IonicModule,
-    CommonModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: SlideshowComponent,
-      },
-    ]),
-  ],
+  imports: [IonicModule, CommonModule],
+  exports: [SlideshowComponent],
 })
 export class SlideshowComponentModule {}
